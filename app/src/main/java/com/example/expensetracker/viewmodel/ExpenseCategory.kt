43dc367b066558e5cd8a8a9e31fcebd69811bd25
Karthik <110925398+Karthik152@users.noreply.github.com>
2026-06-17 21:34:18 +0530

    removed space 2nd time

--- app/src/main/java/com/example/expensetracker/viewmodel/ExpenseCategory.kt
+++ app/src/main/java/com/example/expensetracker/viewmodel/ExpenseCategory.kt
@@ -12,7 +12,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 
 data class ExpenseCategory(
-    val name:  String,
+    val name: String,
     val icon: ImageVector,
     val color: Color
 )
